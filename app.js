ceb03088f845f4b6d4c9286b24a80015b36420f5
Bal Gopal Lal Shrestha <03bgls25@gmail.com>
2022-09-14 10:04:44 +0545

    added chatbot module

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const http = require('http').createServer(app);
 const bodyParser = require("body-parser");
 require("dotenv/config");
 
+const { NlpManager } = require('node-nlp');
+const manager = new NlpManager({ languages: ['en'], nlu: { useNoneFeature: false, log: true }});
+manager.load();
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -30,6 +34,18 @@ io.on('connection', socket => {
             availableFrom: moment().format("hh:mm A")
         }
         users.push(user);
+        let usersId = users.map((obj) => {
+            return obj.id;
+        })
+        
+        let ibot = {
+            id: 'ibotYftM56DjvmsxDReB',
+            name: 'iBot',
+            availableFrom: moment().format("hh:mm A")
+        }
+        if(!usersId.includes(ibot.id)){
+            users.push(ibot);
+        }
         io.emit("user-connected", users);
     })
     socket.on("disconnect", () => {
@@ -46,8 +62,29 @@ io.on('connection', socket => {
     })
     socket.on("chat-message", data => {
         io.to(data.room).emit("message", formatMessage(data.sender, data.message, data.room));
+
+        let room = Buffer.from(data.room, 'base64').toString();
+        if(room.includes("iBot")){
+            chatbot(data.message).then(result => {
+                if(result == null){
+                    setTimeout(() => {
+                        io.to(data.room).emit("message", formatMessage("iBot", "Sorry Contact to Support team...", data.room));
+                    }, 1000);
+                  }
+                  else{
+                    setTimeout(() => {
+                        io.to(data.room).emit("message", formatMessage("iBot", result, data.room));
+                    }, 1000);
+                  }
+            })
+        }
     })
 })
+
+async function chatbot(msg){
+    let response = await manager.process("en", msg);
+    return response.answer;
+}
 
 function formatMessage(user, message, room) {
     return {
